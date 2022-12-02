d359cb9c4ad936d7a3a6d31bd7db70e5332b0acc
Danial <85788131+danialjankouk@users.noreply.github.com>
2022-12-02 20:44:39 +0330

    better image align

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,12 +9,18 @@ const Landing = () => {
     }
     fetchApi()
   },[])
+  function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
   return (
-    <div className="grid grid-cols-6 gap-4 rounded-xl mt-10">
+    <div className="grid grid-cols-6 gap-4 mt-5">
       {data.map((img)=>{
         return(
-          <div key={img.id} className="flex flex-col justify-around">
-            <img src={img.userImageURL} alt={img.tags} className=""/>  
+          <div key={img.id} className="flex h-[20rem] p-1 flex-col justify-around">
+            <img src={img.userImageURL} alt={img.tags} className={`rounded-xl`} height={randomInt(30,800)}/>  
             <p>{img.user}</p>
           </div>
         )
